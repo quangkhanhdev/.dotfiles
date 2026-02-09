1f5b35dedbf2b3718759a3474d0492f210c6fb60
Quang Khanh <rubykhanhas@gmail.com>
2026-02-09 22:43:47 +0700

    +fix: rofi font emoji

--- rofi/.config/rofi/powermenu.sh
+++ rofi/.config/rofi/powermenu.sh
@@ -7,13 +7,14 @@ uptime="`uptime -p | sed -e 's/up //g'`"
 host=`hostname`
 
 # Options
-shutdown=''
-reboot=''
-lock=''
-suspend=''
-logout=''
-yes=''
-no=''
+shutdown=''
+reboot=''
+lock=''
+suspend=''
+logout=''
+
+yes=''
+no=''
 
 # Rofi CMD
 rofi_cmd() {
